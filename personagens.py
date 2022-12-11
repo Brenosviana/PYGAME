1c74d0374cb148d6e785a55c11a8dd23664c8adf
Luiz Eduardo Vedoato <luizeduardovedoato@MacBook-Air-de-Luiz.local>
2022-12-11 20:52:26 -0300

    first commit

--- personagens.py
+++ personagens.py
@@ -12,25 +12,25 @@ class Personagem:
         if self.personagem == 1:
             
             VELOCIDADE = 0.8
-            VIDA = 40
-            VELOCIDADE_ATQ = 2
-            DANO = 5
+            VIDA = 55
+            VELOCIDADE_ATQ = 6
+            DANO = 4
             self.status = (VELOCIDADE, VIDA, 1, VELOCIDADE_ATQ, DANO)
           
         #CYBORG DANO EM AREA COM MOUSE
         elif self.personagem == 2:
             
             VELOCIDADE = 1.0
-            VIDA = 20
+            VIDA = 15
             VELOCIDADE_ATQ = 2
-            DANO = 4
+            DANO = 8
             self.status = (VELOCIDADE, VIDA, 2, VELOCIDADE_ATQ, DANO)
           
         #TRON MISSEL
         elif self.personagem == 3:
             
-            VELOCIDADE = 1.0
-            VIDA = 30
+            VELOCIDADE = 1.5
+            VIDA = 35
             VELOCIDADE_ATQ = 4
             DANO = 5
             self.status = (VELOCIDADE, VIDA, 3, VELOCIDADE_ATQ, DANO)
@@ -40,7 +40,7 @@ class Personagem:
             
             VELOCIDADE = 1.0
             VIDA = 30
-            VELOCIDADE_ATQ = 1
-            DANO = 3
+            VELOCIDADE_ATQ = 1.4
+            DANO = 4
             self.status = (VELOCIDADE, VIDA, 4, VELOCIDADE_ATQ, DANO)
     
